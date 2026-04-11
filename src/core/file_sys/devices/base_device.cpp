6b78d8ff4868b07616a264291e40b7b7994ca45b
Marek Ledworowski <fotelpl@gmail.com>
2026-04-11 17:55:53 +0200

    a small oopsie

--- src/core/file_sys/devices/base_device.cpp
+++ src/core/file_sys/devices/base_device.cpp
@@ -7,15 +7,17 @@
 
 namespace Core::Devices {
 
+using SeekOrigin = Common::FS::SeekOrigin;
+
 BaseDevice::BaseDevice() = default;
 
 BaseDevice::~BaseDevice() = default;
 
 s64 BaseDevice::lseek(s64 offset, int whence) {
     this->file_offset =
-        ((Common::FS::SeekOrigin::SetOrigin == (u32)whence) * offset) +
-        ((Common::FS::SeekOrigin::CurrentPosition == whence) * (this->file_offset + offset)) +
-        ((Common::FS::SeekOrigin::End == whence) * ((offset > 0) * offset));
+        ((static_cast<int>(SeekOrigin::SetOrigin) == whence) * offset) +
+        ((static_cast<int>(SeekOrigin::CurrentPosition) == whence) * (this->file_offset + offset)) +
+        ((static_cast<int>(SeekOrigin::End) == whence) * ((offset > 0) * offset));
     // ::END is pro-forma
     return this->file_offset >= 0 ? this->file_offset : -POSIX_EINVAL;
 }
